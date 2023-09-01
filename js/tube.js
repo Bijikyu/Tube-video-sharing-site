f136baa93a1fe46d0e6dfff82a2ddcf623e6891a
smhabib99-99 <habibaust34@gmail.com>
2023-09-01 23:24:28 +0600

    dynamic Card Appen Container added and blueprint view done for user's display

--- js/tube.js
+++ js/tube.js
@@ -27,7 +27,54 @@ const handleTubeTab = async (category_id) => {
     `);
 
     const data = await res.json();
-    console.log(data);
+    // Catching dynamic-card-append-container for appending card one by one and then show card in display.
+
+    const dynamicCardAppendContainer = document.getElementById('dynamic-card-append-container');
+
+    // Creating div by forEach for dynamicCardAppendContainer
+
+    data.data.forEach((tube) => {
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+
+        <div class="card-body">
+
+            <div>
+                <!-- <img src="./Logo.png" alt="">
+                <p>a</p> -->
+                <p class="card-title">
+                    <img src="./Icon.png" alt=""> building a winning UX Strategy
+                </p>
+            </div>
+
+            <h2 class="">
+                Awlad hossain
+                <div class="badge badge-xs rounded-full badge-primary">a</div>
+            </h2>
+            <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
+            <!-- <div class="card-actions justify-end">
+                <div class="badge badge-outline">Fashion</div>
+                <div class="badge badge-outline">Products</div>
+            </div> -->
+            <!-- show views -->
+            <p>91k views</p>
+        </div>
+
+    </div>
+    
+    `;
+
+    // Appending card in container for showing in display for user
+
+    dynamicCardAppendContainer.appendChild(div);
+
+    }
+    );
+
+    console.log(data.data);
 
     console.log(category_id);
 }
