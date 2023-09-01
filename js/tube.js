01fbc82c3b81f96a203e2f24d4d9aa6e6ec7c69c
smhabib99-99 <habibaust34@gmail.com>
2023-09-01 19:25:47 +0600

    tab section added

--- js/tube.js
+++ js/tube.js
@@ -1,1 +1,13 @@
-console.log("hello from js");
+const loadAllTubeItems = async() => {
+
+    const allVideos = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+
+    const data = await allVideos.json();
+    console.log(data.data);
+
+
+    // console.log("hello from js");
+
+}
+
+loadAllTubeItems();
